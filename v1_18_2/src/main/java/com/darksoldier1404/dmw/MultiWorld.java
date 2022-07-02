e8ac5f45d1e69a0305968207d7561a1f71e9b582
darksoldier1404 <darksoldier1404@gmail.com>
2022-07-02 21:13:12 +0900

    1.19 update

---
+++ v1_18_2/src/main/java/com/darksoldier1404/dmw/MultiWorld.java
@@ -0,0 +1,38 @@
+package com.darksoldier1404.dmw;
+
+import com.darksoldier1404.dmw.commands.DMWCommand;
+import com.darksoldier1404.dmw.functions.DMWFunction;
+import com.darksoldier1404.dppc.utils.ColorUtils;
+import com.darksoldier1404.dppc.utils.ConfigUtils;
+import org.bukkit.World;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("all")
+public class MultiWorld extends JavaPlugin {
+    public static String prefix;
+    private static MultiWorld plugin;
+    public static YamlConfiguration config;
+    public static Map<String, World> customWorlds = new HashMap<>();
+
+    public static MultiWorld getInstance() {
+        return plugin;
+    }
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+        config = ConfigUtils.loadDefaultPluginConfig(plugin);
+        prefix = ColorUtils.applyColor(config.getString("Settings.prefix"));
+        getCommand("dmw").setExecutor(new DMWCommand());
+        DMWFunction.loadAllCustomWorlds();
+    }
+
+    @Override
+    public void onDisable() {
+        ConfigUtils.savePluginConfig(plugin, config);
+    }
+}
